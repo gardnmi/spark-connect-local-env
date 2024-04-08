44102d72c0ec64f3b2954f99de1ac55f1df2fd73
Michael Gardner <gardnmi@gmail.com>
2024-04-08 03:32:04 +0000

    cleaned up code

---
+++ src/spark_connect_local_env/main.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from datetime import datetime, date
+from pyspark.sql import Row
+from delta import *
+
+
+builder = SparkSession.builder.appName("devcontainer").remote("sc://spark:15002")
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+# Create a DataFrame
+df = spark.createDataFrame(
+    [
+        Row(a=1, b=2.0, c="string1", d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+        Row(a=2, b=3.0, c="string2", d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+        Row(a=4, b=5.0, c="string3", d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0)),
+    ]
+)
+
+# Write Delta Table to Minio
+df.write.mode("overwrite").format("delta").save("s3a://delta-lake/my_table")
+
+# Read Delta Table from Minio
+df = spark.read.format("delta").load("s3a://delta-lake/my_table")
+
+# Display Delta Table
+df.show()
+
+try:
+    DeltaTable.forPath(spark, "s3a://delta-lake/my_table").toDF().show()
+except Exception as e:
+    print(e)
+
+
